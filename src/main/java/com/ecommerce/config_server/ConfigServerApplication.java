10164e3bd8853c9c86afbcd39a1e3f9278d24df3
jorgeArana <72237082+skneyte@users.noreply.github.com>
2026-06-19 19:19:59 -0500

    Update ConfigServerApplication.java

--- src/main/java/com/ecommerce/config_server/ConfigServerApplication.java
+++ src/main/java/com/ecommerce/config_server/ConfigServerApplication.java
@@ -10,9 +10,6 @@ public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("GITHUB_USER=" + System.getenv("GITHUB_USER"));
-		System.out.println("GITHUB_TOKEN_EXISTS=" + (System.getenv("GITHUB_TOKEN") != null));
-
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
