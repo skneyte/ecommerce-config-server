7d0b268df733083548b906000b3b61e100445b68
jorge <jaranare@emeal.nttdata.com>
2026-06-19 19:03:37 -0500

    test

--- src/main/java/com/ecommerce/config_server/ConfigServerApplication.java
+++ src/main/java/com/ecommerce/config_server/ConfigServerApplication.java
@@ -9,6 +9,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("GITHUB_USER=" + System.getenv("GITHUB_USER"));
+		System.out.println("GITHUB_TOKEN_EXISTS=" + (System.getenv("GITHUB_TOKEN") != null));
+
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
